f9e247c0a37cdf69aa05bdfe789093a742c7f47a
Dayana <dr750837@gmail.com>
2023-07-31 08:31:42 -0500

    app3

--- repo.js
+++ repo.js
@@ -1,3 +1,5 @@
 let numero1 = parseFloat(prompt("ingrese el primer numero"))
 let numero2 = parseFloat(prompt("imgrese el segundo numero"))
 
+let promedio = (numero1 + numero2)/2
+alert("el promedio de los numeros es: " + promedio)
